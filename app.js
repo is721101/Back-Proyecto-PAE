093d73908ddd220003bde1514fdb2dbd2fe3564c
is722204 <is722204@iteso.mx>
2021-05-11 06:46:01 -0500

    Finish... almost

--- app.js
+++ app.js
@@ -136,7 +136,20 @@ app.use(async (req, res, next) => {
     res.send(err);
   }
 });
+app.use(function (req, res, next) {
+  next(createError(404));
+});
 
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.redirect("/");
+});
 // http.listen(3000, () => console.log('listening on port 3000'));
 
  http.listen(process.env.PORT || 3000, function(){
