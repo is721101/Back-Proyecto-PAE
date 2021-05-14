966d405db5c41fca01a68e64e8939726bf1d1596
Chris <ChristianFigueroaMontano@outlook.com>
2021-05-14 07:45:52 -0500

    Otro

--- config/s3.env.js
+++ config/s3.env.js
@@ -1,10 +1,10 @@
-const env = {
+/*const env = {
 	AWS_ACCESS_KEY: 'AKIATP7PPZ3LZQIXLDTP', // change to yours
 	AWS_SECRET_ACCESS_KEY: 'H4LMrhGmi8muXPaDYnpqh7JCTD4K3zkK7FlqKvUr', // change to yours
 	REGION : 'us-east-1', // change to yours
 	Bucket: 'node-angular-s3-proyecto' // change to yours
 };
  
-module.exports = env;
+module.exports = env;*/
 
  
