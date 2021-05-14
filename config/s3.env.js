0527a7aef99be8ff5b4670ee5d80ffea88ae65ef
Chris <ChristianFigueroaMontano@outlook.com>
2021-05-14 08:11:51 -0500

    Keys

--- config/s3.env.js
+++ config/s3.env.js
@@ -1,6 +1,6 @@
 const env = {
-	AWS_ACCESS_KEY: 'AKIATP7PPZ3LZQIXLDTP', // change to yours
-	AWS_SECRET_ACCESS_KEY: 'H4LMrhGmi8muXPaDYnpqh7JCTD4K3zkK7FlqKvUr', // change to yours
+	AWS_ACCESS_KEY: 'AKIATP7PPZ3LSXYHET5P', // change to yours
+	AWS_SECRET_ACCESS_KEY: '7PRdCErMTMsjInW4sU13EYx6fxlIChzp8rSaArEJ', // change to yours
 	REGION : 'us-east-1', // change to yours
 	Bucket: 'node-angular-s3-proyecto' // change to yours
 };
