0d56bb05557061137da2417f21afc064facf0243
Chris <ChristianFigueroaMontano@outlook.com>
2021-05-14 07:50:28 -0500

    Otro push

--- config/s3.config.js
+++ config/s3.config.js
@@ -5,7 +5,7 @@ const s3 = new AWS.S3({
     accessKeyId: process.env.AWS_ACCESS_KEY,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
 	region : process.env.REGION,
-    Bucket: process.env.process.BUCKET// change to yours
+    Bucket: process.env.BUCKET// change to yours
 });
 
  
