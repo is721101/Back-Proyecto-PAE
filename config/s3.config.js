d82f7f957ef89b2dd511ce9e63a7176119f078a4
Chris <ChristianFigueroaMontano@outlook.com>
2021-05-14 07:22:41 -0500

    Porque uploader, porque?

--- config/s3.config.js
+++ config/s3.config.js
@@ -2,9 +2,10 @@ const AWS = require('aws-sdk');
 const env = require('./s3.env.js');
  
 const s3 = new AWS.S3({
-    accessKeyId: env.AWS_ACCESS_KEY,
-    secretAccessKey: env.AWS_SECRET_ACCESS_KEY,
-	region : env.REGION
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+	region : process.env.REGION,
+    Bucket: process.env.process.BUCKET// change to yours
 });
 
  
